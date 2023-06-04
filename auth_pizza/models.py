2950dc4572dd225cc5fc57fbb0ec2884b3435790
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-06-04 10:08:55 -0400

    se crea modelo de usuario

--- auth_pizza/models.py
+++ auth_pizza/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    nombre = models.CharField('Nombre', max_length=300)
+
+    class Meta:
+        ordering = ['-is_active', 'username']
+
+    def __str__(self):
+        if not self.nombre:
+            return self.username
+        return self.nombre
+
+    def save(self, *args, **kwargs):
+        self.username = self.email
+        super().save(*args, **kwargs)
+
