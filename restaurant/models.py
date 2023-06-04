4bf73df928111e7a5e81343ddddf2f24d2c23b3d
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-06-04 10:45:09 -0400

    se agrega modelo de pizza e ingrediente y su migration

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Pizza(models.Model):
+    nombre = models.CharField('Nombre', max_length=250)
+    precio = models.PositiveSmallIntegerField('Precio')
+    activo = models.BooleanField('Activo', default=True)
+    ingredientes = models.ManyToManyField('Ingrediente', related_name='ingredientes', verbose_name='Ingredientes')
+
+    def __str__(self):
+        return self.nombre
+
+
+class Ingrediente(models.Model):
+
+    CATEGORIAS = (
+        (10, 'Basico'),
+        (20, 'Premium')
+    )
+
+    nombre = models.CharField('Nombre', max_length=250)
+    categoria = models.PositiveSmallIntegerField('Categoria', choices=CATEGORIAS)
+
+    def __str__(self):
+        return self.nombre
