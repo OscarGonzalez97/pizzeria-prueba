51462bcf9aae4c6dd18e115170b33f758fa58687
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-06-04 11:22:05 -0400

    se agregan modelos al admin para pruebas

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Pizza, Ingrediente
 
-# Register your models here.
+
+@admin.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio')
+
+
+@admin.register(Ingrediente)
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria')
